2fb2483426606c64a7888761c856ae5542fcb0b7
Asia Thompson <asiasatwork@gmail.com>
2026-02-09 11:51:30 -0800

    Add pages for the home website and update the navigation

--- components/layout/home-nav.tsx
+++ components/layout/home-nav.tsx
@@ -1,26 +1,43 @@
 import React from 'react'
 import Link from 'next/link'
+import { Button } from '../ui/button'
 
 const navItems = [
-    { href: '/', label: 'Dashboard'},
-    { href: '/habits', label: 'Habits'},
-    { href: '/tasks', label: 'Tasks'},
-    { href: '/projects', label: 'Projects'},
+    { href: '/features', label: 'Features'},
+    { href: '/pricing', label: 'Pricing'},
+    { href: '/resources', label: 'Resources'},
+    { href: '/about', label: 'About'},
   ]
 
 const HomeNav = () => {
   return (
-    <div>
-        <nav className='flex gap-4'>
-        {navItems.map((item) => (
-            <Link 
-              key={item.href}
-              href={item.href}
-            >
-                {item.label}
-            </Link>
-          ))}
-        </nav>
+    <div className='w-full pl-5 pr-10 py-4 flex content-center justify-between'>
+        <Link href='/' className='block w-24 h-auto'>
+          <img 
+            src="/Life-Levels-Logo.svg" 
+            alt="Life Levels Logo" 
+          />
+        </Link>
+        <div className='flex place-items-center'>
+        <nav className='flex gap-4 place-items-center'>
+            {navItems.map((item) => (
+                <Link 
+                  key={item.href}
+                  href={item.href}
+                >
+                    {item.label}
+                </Link>
+              ))}
+            </nav>
+          <div className='pl-10 flex gap-4'>
+            <Button variant={'outline'}>
+              Log In
+            </Button>
+            <Button>
+              Sign Up for Free
+            </Button>
+          </div>
+        </div>
     </div>
   )
 }
